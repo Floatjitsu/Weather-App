db2aefe10bdfe3345f55bde582b75f305ab84eea
Timur Celik <timur.celik@gmx.de>
2020-01-18 00:05:05 +0100

    worked more on the error message box

--- src/WeatherComponent.jsx
+++ src/WeatherComponent.jsx
@@ -1,33 +1,63 @@
 import React from 'react';
 import Fade from '@material-ui/core/Fade';
 import weatherData from './weatherData';
+import SentimentDissatisfiedRoundedIcon from '@material-ui/icons/SentimentDissatisfiedRounded';
 
 class WeatherComponent extends React.Component {
-	state = {
-		temperatureNow: '35 °C'
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedCity: this.props.value,
+			error: 'TestError',
+			temperatureNow: '35 °C'
+		}
 	}
 
-	componentDidUpdate = () => {
-		this.setTemperatureForCurrentCity();
+	componentDidUpdate = (prevProps) => {
+		if (this.props.value !== prevProps.value) {
+			this.setTemperatureForCurrentCity().then()
+				.catch(error => {
+					console.log(error);
+					this.setState({
+						error: error
+					});
+				});
+		}
 	}
 
 	componentDidMount = () => {
-		this.setTemperatureForCurrentCity();
+		this.setTemperatureForCurrentCity().then()
+			.catch(error => {
+				this.setState({
+					error: error
+				});
+			});
 	}
 
 	setTemperatureForCurrentCity = () => {
-		weatherData(this.props.value).then(result => {
-			this.setState({
-				temperatureNow: result.toFixed(1) + ' °C'
+		return new Promise((resolve, reject) => {
+			weatherData(this.props.value).then(result => {
+				this.setState({
+					temperatureNow: result.toFixed(1) + ' °C'
+				});
+				resolve();
+			}).catch(error => {
+				reject(error);
 			});
-		}).catch(error => {
-			console.log(error);
-		})
+		});
 	}
 
 	render() {
 		return (
 			<div className='weatherComponentContainer'>
+				{this.state.error &&
+					<div className='errorBox'>
+						<SentimentDissatisfiedRoundedIcon />
+						<div style={{marginLeft: 10}}>
+							{this.state.error}
+						</div>
+					</div>
+				}
 				<div className='weatherComponentCity'>
 					<Fade in={true} timeout={900}>
 						<p>{this.props.value}</p>
--- src/weatherData.js
+++ src/weatherData.js
@@ -6,10 +6,10 @@ const apiKey = 'appid=63adac7ad2e70d880b81ccda4407faeb';
 const getCurrentWeatherForCity = city => {
 	return new Promise((resolve, reject) => {
 		request(apiUrl + '&q=' + city + '&' + apiKey, (error, response, body) => {
-			if (!error) {
+			if (response.statusCode === 200) {
 				resolve(JSON.parse(body).main.temp);
 			} else {
-				reject(error);
+				reject(response.statusMessage);
 			}
 		});
 	});
