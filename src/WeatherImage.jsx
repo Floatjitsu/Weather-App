26ce758d0356ba6f9a5ae8178510d4f170d8cd70
Timur Celik <timur.celik@gmx.de>
2020-01-18 11:26:03 +0100

    Added more Weather scenario svg's

--- src/WeatherImage.jsx
+++ src/WeatherImage.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
-import {ReactComponent as SunnyImage} from './weatherImages/sun.svg';
-import {ReactComponent as RainyImage} from './weatherImages/rainy.svg';
+import RainyImage from './weatherImages/rainy.svg';
+import SunnyImage from './weatherImages/sun.svg';
+import SnowImage from './weatherImages/snow.svg';
+import CloudyImage from './weatherImages/cloudy.svg';
 
-export default class ClassName extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+const imageStyle = {height: 100, width: 100};
 
+export default class WeatherImage extends React.Component {
 	render() {
-		return (
-			<div>
-				<p>{this.props.value}</p>
-			</div>
-		);
+		switch (this.props.value) {
+			case 'Rain':
+				return (
+					<img src={RainyImage}
+						 style={imageStyle}
+						 alt={this.props.value} />
+				 );
+			case 'Clear':
+				return (
+					<img src={SunnyImage}
+						 style={imageStyle}
+						 alt={this.props.value} />
+				 );
+			case 'Snow':
+				return (
+					<img src={SnowImage}
+						 style={imageStyle}
+						 alt={this.props.value} />
+				);
+			case 'Clouds':
+				return (
+					<img src={CloudyImage}
+						 style={imageStyle}
+						 alt={this.props.value} />
+				);
+			default:
+				return (
+				 	<div>
+				 		<p>{this.props.value}</p>
+				 	</div>
+				);
+		}
+
 	}
 }
