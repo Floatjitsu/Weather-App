08bd6b9cd2a88dd229cf3cc93d48280bc9a75531
Timur Celik <timur.celik@gmx.de>
2020-01-28 16:00:16 +0100

    added contact to about page

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
+import EmailIcon from '@material-ui/icons/Email';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import DevPic from '../images/dev-avatar.jpg';
 
 export default class AboutComponent extends React.Component {
@@ -11,10 +13,26 @@ export default class AboutComponent extends React.Component {
 					<li className='aboutListItem'>
 						<div style={{fontSize: 20}}> Creator </div>
 					</li>
-					<hr className='aboutListDivider'/>
+					<hr className='aboutListDivider' />
 					<li className='aboutListItem'>
 						<Avatar src={DevPic} alt='Timur Celik'/>
 						<div style={{marginLeft: 10}}> Timur Celik </div>
+					</li>
+					<li className='aboutListItem'>
+						<div style={{fontSize: 20, marginTop: 20}}> Contact </div>
+					</li>
+					<hr className='aboutListDivider' />
+					<li className='aboutListItem'>
+						<GitHubIcon />
+						<div style={{marginLeft: 10}}>
+							 <a href='https://github.com/Floatjitsu'>
+								 Floatjitsu
+						 	 </a>
+						 </div>
+					</li>
+					<li className='aboutListItem'>
+						<EmailIcon />
+						<div style={{marginLeft: 10}}> timur.celik@gmx.de </div>
 					</li>
 				</ul>
 			</div>
