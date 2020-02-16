7f9a290874a1c735d264ceb14b27f35cb3c44a85
Timur Celik <timur.celik@gmx.de>
2020-02-16 15:49:29 +0100

    refactored weather forecast time list

--- src/components/WeatherForecastTimes.jsx
+++ src/components/WeatherForecastTimes.jsx
@@ -2,65 +2,37 @@ import React from 'react';
 import {List, ListItem, Divider, ListItemText} from '@material-ui/core';
 import WeatherImage from './WeatherImage';
 
+function ForecastListItem(props) {
+	return (Object.keys(props.value).length === 0 ?
+		null :
+		<div>
+			<ListItem key={'threeAm'}>
+				<div style={{marginRight: 15}}>
+					{props.value.time}
+				</div>
+				<div style={{marginRight: 15}}>
+					<WeatherImage
+						value={props.value.weatherMain}
+						imageStyle={{height: 35, width: 35}} />
+				</div>
+				<ListItemText
+					primary={props.value.temperature}
+					secondary={props.value.description} />
+			</ListItem>
+			<Divider />
+		</div>
+	);
+
+}
+
 class WeatherForecastTimes extends React.Component {
 	render() {
 		return (
 			<List>
-				<ListItem key={'threeAm'}>
-					<div style={{marginRight: 15}}>
-						{this.props.forecastList.threeAm.time}
-					</div>
-					<div style={{marginRight: 15}}>
-						<WeatherImage
-							value={this.props.forecastList.threeAm.weatherMain}
-							imageStyle={{height: 35, width: 35}} />
-					</div>
-					<ListItemText
-						primary={this.props.forecastList.threeAm.temperature}
-						secondary={this.props.forecastList.threeAm.description} />
-				</ListItem>
-				<Divider />
-				<ListItem key={'nineAm'}>
-					<div style={{marginRight: 15}}>
-						{this.props.forecastList.nineAm.time}
-					</div>
-					<div style={{marginRight: 15}}>
-						<WeatherImage
-							value={this.props.forecastList.nineAm.weatherMain}
-							imageStyle={{height: 35, width: 35}} />
-					</div>
-					<ListItemText
-						primary={this.props.forecastList.nineAm.temperature}
-						secondary={this.props.forecastList.nineAm.description} />
-				</ListItem>
-				<Divider />
-				<ListItem key={'threePm'}>
-					<div style={{marginRight: 15}}>
-						{this.props.forecastList.threePm.time}
-					</div>
-					<div style={{marginRight: 15}}>
-						<WeatherImage
-							value={this.props.forecastList.threePm.weatherMain}
-							imageStyle={{height: 35, width: 35}} />
-					</div>
-					<ListItemText
-						primary={this.props.forecastList.threePm.temperature}
-						secondary={this.props.forecastList.threePm.description} />
-				</ListItem>
-				<Divider />
-				<ListItem key={'ninePm'}>
-					<div style={{marginRight: 15}}>
-						{this.props.forecastList.ninePm.time}
-					</div>
-					<div style={{marginRight: 15}}>
-						<WeatherImage
-							value={this.props.forecastList.ninePm.weatherMain}
-							imageStyle={{height: 35, width: 35}} />
-					</div>
-					<ListItemText
-						primary={this.props.forecastList.ninePm.temperature}
-						secondary={this.props.forecastList.ninePm.description} />
-				</ListItem>
+				<ForecastListItem value={this.props.forecastList.threeAm}/>
+				<ForecastListItem value={this.props.forecastList.nineAm}/>
+				<ForecastListItem value={this.props.forecastList.threePm}/>
+				<ForecastListItem value={this.props.forecastList.ninePm}/>
 			</List>
 		);
 	}
